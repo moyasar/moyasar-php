34c7b19df5aee068562b7c853b6aab936ccfdd2b
nuha <nuhaaziz75@gmail.com>
2019-03-18 12:45:53 +0300

    new version for create() forbidding

--- src/Moyasar/Invoice.php
+++ src/Moyasar/Invoice.php
@@ -15,17 +15,6 @@ class Invoice
     const CURRENCY = "currency";
     const DESCRIPTION = "description";
 
-    public static function create($amount, $description, $currency = "SAR")
-    {
-
-        $data = [
-            self::AMOUNT => $amount,
-            self::DESCRIPTION => $description,
-            self::CURRENCY => $currency
-        ];
-
-        return json_decode(Client::post("https://api.moyasar.com/v1/invoices", $data));
-    }
 
     public static function fetch($id){
         return json_decode(Client::get("https://api.moyasar.com/v1/invoices/$id"));
