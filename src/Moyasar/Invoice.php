51c58bfd5d52761b668e617a8ec79a5f51db341c
nuha <nuhaaziz75@gmail.com>
2019-03-18 14:42:31 +0300

    re-add create() for Invoice

--- src/Moyasar/Invoice.php
+++ src/Moyasar/Invoice.php
@@ -11,6 +11,21 @@ namespace Moyasar;
 
 class Invoice
 {
+    const AMOUNT = "amount";
+    const CURRENCY = "currency";
+    const DESCRIPTION = "description";
+
+    public static function create($amount, $description, $currency = "SAR")
+    {
+
+        $data = [
+            self::AMOUNT => $amount,
+            self::DESCRIPTION => $description,
+            self::CURRENCY => $currency
+        ];
+
+        return json_decode(Client::post("https://api.moyasar.com/v1/invoices", $data));
+    }
 
     public static function fetch($id){
         return json_decode(Client::get("https://api.moyasar.com/v1/invoices/$id"));
