77e67730ea96865b77359abdcce2b41dfa50121c
Ali Alhoshaiyan <alialhoshaiyan@gmail.com>
2020-12-28 12:12:42 +0300

    Upgrade GuzzleHttp Version

--- src/Moyasar.php
+++ src/Moyasar.php
@@ -4,7 +4,7 @@ namespace Moyasar;
 
 class Moyasar
 {
-    const VERSION = '1.0.1';
+    const VERSION = '1.1.0';
     const API_BASE_URL = 'https://api.moyasar.com';
     const API_VERSION = 'v1';
     const CURRENT_VERSION_URL = self::API_BASE_URL . '/' . self::API_VERSION . '/';
